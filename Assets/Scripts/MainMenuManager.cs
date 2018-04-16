0553913829bbe5e0ff072e98f581d9e428844ed3
Taurean Boyd <trboyd8@gmail.com>
2018-04-16 19:43:29 -0400

    Adding a main menu scene.

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+    public void Play()
+    {
+        // '1' is the index of the game scene
+        SceneManager.LoadScene(1);
+    }
+
+    public void Instructions()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MenuLineRenderer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuLineRenderer : MonoBehaviour {
+
+    private LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start () {
+        lineRenderer = this.GetComponentInChildren<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lineRenderer.SetPosition(0, this.transform.position);
+        lineRenderer.SetPosition(1, this.transform.position + this.transform.forward * 3.47f);
+	}
+}
