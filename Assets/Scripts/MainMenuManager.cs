4fe0f16f8c3ae3b3c08be930a387beacdbd9a91b
Taurean Boyd <trboyd8@gmail.com>
2018-04-16 20:13:17 -0400

    Updating the main menu.

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour {
 
+    public GameObject MainScreen;
+    public GameObject InstructionsScreen;
+    public GameObject ControlsScreen;
+
     public void Play()
     {
         // '1' is the index of the game scene
@@ -13,11 +17,24 @@ public class MainMenuManager : MonoBehaviour {
 
     public void Instructions()
     {
-
+        MainScreen.SetActive(false);
+        InstructionsScreen.SetActive(true);
     }
 
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void Menu()
+    {
+        MainScreen.SetActive(true);
+        ControlsScreen.SetActive(false);
+    }
+
+    public void Controls()
+    {
+        InstructionsScreen.SetActive(false);
+        ControlsScreen.SetActive(true);
     }
 }
