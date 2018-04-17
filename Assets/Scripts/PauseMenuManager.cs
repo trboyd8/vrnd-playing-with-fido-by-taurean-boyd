d072a0db8433730929bf27d4a9320e46e4eb3544
Taurean Boyd <trboyd8@gmail.com>
2018-04-16 21:00:49 -0400

    Adding a pause menu.

---
+++ Assets/Scripts/PauseMenuManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PauseMenuManager : MonoBehaviour {
+
+    public static bool IsGamePaused = false;
+    public GameObject PauseMenu;
+    public GameObject Player;
+
+    private void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Start))
+        {
+            if (IsGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        IsGamePaused = false;
+        Time.timeScale = 1f;
+        PauseMenu.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    private void Pause()
+    {
+        IsGamePaused = true;
+        Time.timeScale = 0f;
+        PauseMenu.transform.position = new Vector3(Player.transform.position.x + Player.transform.forward.x * 1.5f, Player.transform.position.y, Player.transform.position.z + Player.transform.forward.z * 1.5f);
+        PauseMenu.transform.LookAt(Player.transform.position);
+        PauseMenu.transform.Rotate(new Vector3(0, 180, 0));
+        PauseMenu.SetActive(true);
+    }
+}
