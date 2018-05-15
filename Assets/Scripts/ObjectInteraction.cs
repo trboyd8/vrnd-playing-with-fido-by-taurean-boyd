723c56d53a626ec20898e00e2a2b087f86d5ea4e
Taurean Boyd <trboyd8@gmail.com>
2018-05-15 15:16:53 -0400

    Adding object interaction.

---
+++ Assets/Scripts/ObjectInteraction.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class ObjectInteraction : MonoBehaviour {
+
+    public OVRInput.Controller thisController;
+    private float throwForce = 2f;
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("You hit an object");
+        if (other.CompareTag("Grabbable"))
+        {
+            if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, thisController))
+            {
+                GrabObject(other);
+            }
+            else if (!OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, thisController))
+            {
+                ThrowObject(other);
+            }
+        }
+
+        if (other.CompareTag("Dog"))
+        {
+            // Implement Petting
+        }
+    }
+
+    private void GrabObject(Collider other)
+    {
+        other.transform.SetParent(gameObject.transform);
+        other.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    private void ThrowObject(Collider other)
+    {
+        other.transform.SetParent(null);
+        Rigidbody rigidbody = other.GetComponent<Rigidbody>();
+        rigidbody.isKinematic = false;
+        rigidbody.velocity = OVRInput.GetLocalControllerVelocity(thisController) * throwForce;
+        rigidbody.angularVelocity = OVRInput.GetLocalControllerAngularVelocity(thisController);
+    }
+}
